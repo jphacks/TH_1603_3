d66015ba0959c570a22bd95cdda1bad4946d5792
Sota Yano <kuroisiratama@yahoo.co.jp>
2016-10-30 02:35:04 +0000

    merged weather

--- 2016jphacks.py
+++ 2016jphacks.py
@@ -1,15 +1,22 @@
 #coding: utf-8
-import urllib
-import json
-import random
-import string
-import requests
-import numpy
-import cv2
-import subprocess
+import urllib,json,random,string,requests,numpy,cv2,subprocess,urllib,urllib2,re
 from datetime import datetime
 import feedparser
 
+def jtalk2(t):
+    open_jtalk=['open_jtalk']
+    mech=['-x','/var/lib/mecab/dic/open-jtalk/naist-jdic']
+    htsvoice=['-m','/usr/share/hts-voice/mei/mei_normal.htsvoice']
+    speed=['-r','0.8']
+    outwav=['-ow','open_jtalk2.wav']
+    cmd=open_jtalk+mech+htsvoice+speed+outwav
+    c = subprocess.Popen(cmd,stdin=subprocess.PIPE)
+    c.stdin.write(t)
+    c.stdin.close()
+    c.wait()
+    aplay = ['aplay','-q','open_jtalk2.wav']
+    wr = subprocess.Popen(aplay)
+    
 def jtalk(t):
     open_jtalk=['open_jtalk']
     mech=['-x','/var/lib/mecab/dic/open-jtalk/naist-jdic']
@@ -24,9 +31,10 @@ def jtalk(t):
     aplay = ['aplay','-q','open_jtalk.wav']
     wr = subprocess.Popen(aplay)
 
-def say_datetime():
+def say_datetime(isMorning):
     d = datetime.now()
-    text = '%s月%s日、%s時%s分%s秒' % (d.month, d.day, d.hour, d.minute, d.second)
+    text = '%s月%s日、%s時%s分、' % (d.month, d.day, d.hour, d.minute)
+    say_weather(isMorning,text)
 
 def say_news():
     text = 'プロ野球・日本シリーズの第６戦が２９日、マツダスタジアムで行われ、日本ハムが１０－４で広島を破り、１０年ぶりの日本一に輝いた。'
@@ -34,8 +42,6 @@ def say_news():
 
 def say_hello():
     text = 'こんにちは！あなたは...'
-
-    
     jtalk(text)
 
 def say_who():
@@ -46,6 +52,36 @@ def say_hi(name):
     text = name+'さんですね？！こんにちは！！今日のニュースをお届けします。'
     text += descs[6].encode('utf_8');
     jtalk(text)
+
+def say_weather(isMorning,  text):
+    if(isMorning):
+        if(weather_data['query']['results']['channel']['item']['forecast'][0]['text'].find('Partly Cloudy') > -1):
+            jtalk2(text+'今日の天気は晴れ時々曇りです。')
+        if(weather_data['query']['results']['channel']['item']['forecast'][0]['text'].find('Mostly Cloudy') > -1):
+            jtalk2(text+'今日の天気は曇りのち晴れです。')
+        if(weather_data['query']['results']['channel']['item']['forecast'][0]['text'].find('Mostly Sunny') > -1):
+            jtalk2(text+'今日は晴れのち曇りです！')
+        if(weather_data['query']['results']['channel']['item']['forecast'][0]['text'].find('Sunny') > -1):
+            jtalk2(text+'今日は晴れですよ！')
+        if(weather_data['query']['results']['channel']['item']['forecast'][0]['text'].find('Scattered Showers') > -1):
+            jtalk2(text+'今日は雨が少し降ります。傘を持って行きましょう！')
+        if(weather_data['query']['results']['channel']['item']['forecast'][0]['text'].find('Heary rain') > -1):
+            jtalk2(text+'今日はたくさん雨が降ります。傘を持って行きましょう！気をつけて行ってらっしゃいませ。')
+    else:
+        if(weather_data['query']['results']['channel']['item']['forecast'][1]['text'].find('Partly Cloudy') > -1):
+            jtalk2(text+'明日の天気は晴れ時々曇りです。')
+        if(weather_data['query']['results']['channel']['item']['forecast'][1]['text'].find('Mostly Cloudy') > -1):
+            jtalk2(text+'明日の天気は曇りのち晴れです。')
+        if(weather_data['query']['results']['channel']['item']['forecast'][1]['text'].find('Mostly Sunny') > -1):
+            jtalk2(text+'明日は晴れのち曇りです！')
+        if(weather_data['query']['results']['channel']['item']['forecast'][1]['text'].find('Sunny') > -1):
+            jtalk2(text+'明日は晴れですよ！')
+        if(weather_data['query']['results']['channel']['item']['forecast'][1]['text'].find('Scattered Showers') > -1):
+            jtalk2(text+'明日は雨が少し降ります。傘を持って行きましょう！')
+        if(weather_data['query']['results']['channel']['item']['forecast'][1]['text'].find('Hearay rain') > -1):
+            jtalk2(text+'明日はたくさん雨が降ります。傘を持って行きましょう！気をつけて行ってらっしゃいませ。')
+
+
     
     
 def checkface(image_path):  
@@ -71,6 +107,7 @@ def checkface(image_path):
                                 if(name == 'yano'):  say_name = '矢野'
                                 elif(name == 'kudo'): say_name = '工藤'
                                 elif(name == 'nagai'): say_name = '永井'
+                                say_datetime(True)
                                 say_hi(say_name)
                                 cv2.waitKey(30000)
                         else:
@@ -90,6 +127,15 @@ def send():
 	image_path = url+'img/'+filename
 	checkface(image_path)
 
+
+#weather
+baseurl = "https://query.yahooapis.com/v1/public/yql?"
+yql_query = 'select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="sendai")'
+yql_url = baseurl + urllib.urlencode({'q':yql_query}) + "&format=json"
+result = urllib2.urlopen(yql_url.decode('UTF_8')).read()
+weather_data = json.loads(result)
+
+#news
 
 RSS_URL = "http://feed.rssad.jp/rss/news24/index.rdf"
 news_dic = feedparser.parse(RSS_URL)
@@ -114,7 +160,7 @@ while True:
         if len(facerect) <= 0:
                 face_flag = False
                 print("Anyone disappeared")
-                say_datetime()
+                #say_datetime()
 
         if len(facerect) > 0 and face_flag == False:
                 face_flag = True
