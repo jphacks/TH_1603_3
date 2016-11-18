65b0e6bdb06da5c60ca94e8f1983688fb2246bd4
Sota Yano <kuroisiratama@yahoo.co.jp>
2016-11-18 16:23:11 +0900

    Modify key

--- 2016jphacks.py
+++ 2016jphacks.py
@@ -56,7 +56,7 @@ def say_who():
 
 def say_hi(user):
     text = user['name']+'さんですね？！こんにちは！！今日のニュースをお届けします。'
-    text += descs[index_number].encode('utf_8')
+    text += descs[user.topic_number].encode('utf_8')
     if(requests.get('https://rti-giken.jp/fhc/api/train_tetsudo/delay.json').text.find(user.address.decode('utf-8')) > -1):
         text += user.address+'に遅れが出ています。早めに家を出ましょう'
         print 'okureteru!'
